7620f96c431e3c92ba4a43db7508cd7eeb8fccf0
Kasia Lach <lachkasia4@gmail.com>
2020-04-24 00:16:18 +0200

    Updates

---
+++ precache-manifest.35abc06827ee94a8a5fb5010fce2e9c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927369aec1498d409cc9fb979b766904",
+    "url": "/css-charts/index.html"
+  },
+  {
+    "revision": "c1252af86557a84b4051",
+    "url": "/css-charts/static/css/main.7ff419eb.chunk.css"
+  },
+  {
+    "revision": "480b09e11b0089d12c55",
+    "url": "/css-charts/static/js/2.867c48e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/css-charts/static/js/2.867c48e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1252af86557a84b4051",
+    "url": "/css-charts/static/js/main.0484514b.chunk.js"
+  },
+  {
+    "revision": "7898a2e5e312e9299eb5",
+    "url": "/css-charts/static/js/runtime-main.7334f84e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/css-charts/static/media/logo.5d5d9eef.svg"
+  }
+]);
